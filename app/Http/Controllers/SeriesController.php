17d449099eea15ffc902c219346074e0f388d342
rubemeslley <rubemeslley@log-party.com>
2020-08-12 10:24:01 -0300

    first commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use App\Http\Requests\SeriesFormRequest;
+use App\Serie;
+use Illuminate\Http\Request;
+
+class SeriesController extends  Controller
+{
+    public function index(Request $request){
+        $series = Serie::query()->orderBy('nome')->get();
+
+        $mensagem = $request->session()->get('mensagem');
+
+        return view('series.index', compact('series', 'mensagem'));
+    }
+
+    public function create ()
+    {
+        return view('series.create');
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        $serie = Serie::create(['nome' => $request->nome]);
+
+        $qtd_temporadas = $request->qtd_temporadas;
+
+        for( $i = 1; $i <= $qtd_temporadas; $i++){
+            $temporada = $serie->temporadas()->create(['numero' => $i]);
+
+            for( $j= 1; $j <= $request->ep_por_temporada; $j++){
+                $temporada->episodios()->create(['numero' => $j]);
+            }
+        }
+
+        $request->session()->flash('mensagem', "Série id = {$serie->id} adicionada");
+
+        return redirect()->route('listar_series');
+    }
+
+    public function destroy(Request $request)
+    {
+        Serie::destroy($request->id);
+
+        $request->session()->flash('mensagem', "Série id = {$request->id} deletada");
+
+        return redirect()->route('listar_series');
+    }
+}
